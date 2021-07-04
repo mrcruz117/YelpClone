f663c98915dd8e5bd1a556434081e4858dcdf7bb
Michael Cruz <mrcruz1989@gmail.com>
2021-07-04 18:41:28 -0400

    stubbed out a couple more routes

--- server/server.js
+++ server/server.js
@@ -3,12 +3,25 @@ const express = require('express');
 
 const app = express();
 
+// get all restaurants
 app.get('/restaurants', (req, res) => {
-  res.status(202).json({
+  res.status(200).json({
     status: 'success',
-    restaurant: 'Mcdonalds',
+    data: {
+      restaurants: 'Mcdonalds',
+    }
   });
 });
+
+// Get specific restaurant
+app.get('/restaurants/:id',(req,res)=>{
+  console.log(req);
+});
+
+// create a restaurant
+app.post('/restaurants', (req, res) => {
+  console.log(req)
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
