7caaf085410d289097221eae6dfb9167f9e9112e
Michael Cruz <mrcruz1989@gmail.com>
2021-07-04 17:34:52 -0400

    stubbed out first GET route

--- server/server.js
+++ server/server.js
@@ -2,8 +2,15 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3001;
 
+app.get('/restaurants', (req, res) => {
+  res.status(202).json({
+    status: 'success',
+    restaurant: 'Mcdonalds',
+  });
+});
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}/`);
 });
