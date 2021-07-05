b07b1eda53928b7706feb66bb11c3bb3e1574098
Michael Cruz <mrcruz1989@gmail.com>
2021-07-05 16:33:22 -0400

    stubbed out all routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
+
+// app.use(morgan('dev'));
+app.use(express.json());
 
 // get all restaurants
 app.get('/restaurants', (req, res) => {
@@ -9,18 +12,49 @@ app.get('/restaurants', (req, res) => {
     status: 'success',
     data: {
       restaurants: 'Mcdonalds',
-    }
+    },
   });
 });
 
 // Get specific restaurant
-app.get('/restaurants/:id',(req,res)=>{
+app.get('/restaurants/:id', (req, res) => {
   console.log(req);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      restaurants: 'Mcdonalds',
+    },
+  });
 });
 
 // create a restaurant
 app.post('/restaurants', (req, res) => {
-  console.log(req)
+  console.log(req);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      restaurants: 'Mcdonalds',
+    },
+  });
+});
+
+// update restaurant
+app.put('/restaurants/:id', (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      restaurants: 'Mcdonalds',
+    },
+  });
+});
+
+// delete restaurant
+app.delete('/restaurants/:id', (req,res)=>{
+  res.status(204).json({
+    status: 'deleted',
+  })
 })
 
 const port = process.env.PORT || 3001;
